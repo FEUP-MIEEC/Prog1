cf1b53314d4c5c64cbe2a22a4408d64abb6fd3bd
G-Pereira <goncalo_pereira@outlook.pt>
2015-11-11 15:20:02 +0000

    Aula 2 - Porblema 1 Resolvido!

--- Aula_Pratica_02/Problema_1.c
+++ Aula_Pratica_02/Problema_1.c
@@ -1,28 +1,27 @@
 /*
  *  Created on: 28/10/2015
- *      Author: Gonçalo Pereira
+ *      Author: Gonçalo Pereira, Daniel Silva
  */
-// AINDA NÃO FOI ENCONTRADA SOLUÇÃO PARA ESTE PROBLEMA
 #include <stdio.h>
 #include <math.h>
 
 float f_res_sqrt(float a, float b, float c) {
-int tmp = -4 * a * c;
-tmp = b - tmp;
+float tmp = -4 * a * c;
+tmp = b*b + tmp; // Tinha erro na fórmula do binómio discriminante
 tmp = sqrt(tmp);
 return tmp;
 }
 
 float f_res_pos(float a, float b, float c) {
-return (-b + f_res_sqrt(a, b, c)) / 2 * a;
+return (-b + f_res_sqrt(a, b, c)) / (2 * a);
 }
 
 float f_res_neg(float a, float b, float c) {
-return (-b - f_res_sqrt(a, b, c)) / 2 * a;
+return (-b - f_res_sqrt(a, b, c)) / (2 * a);
 }
 
 int main() {
-float PRECISION=0.00001, A=30.4, B=-123, C=75, R_NEG=0.748063f, R_POS=3.29799;
+float PRECISION=0.00001, A=30.4, B=-123, C=75, R_NEG=0.748063, R_POS=3.29799;
 float v_pos = f_res_pos(A, B, C);
 float v_neg = f_res_neg(A, B, C);
 
