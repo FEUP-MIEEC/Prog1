607ecc47aa73b326f6f93bdffa7b55b97797670a
Daniel Silva <daniel_p_silva_97@hotmail.com>
2015-11-13 11:04:37 +0000

    Ajustes de indentação

--- Aula_Pratica_06/Problema_3.c
+++ Aula_Pratica_06/Problema_3.c
@@ -11,23 +11,23 @@ void calc(int x, int y){
 
 void intswap(int *a, int *b){
       /*
-      *Esta função utiliza o conceito de apontadores
-      *para trocar os valores das variáveis
+      * Esta função utiliza o conceito de apontadores
+      * para trocar os valores das variáveis
       * através de operações sobre estas.
       */
 
-  *b=*b+*a;
+	*b=*b+*a;
 	*a=*b-*a;
 	*b=*b-*a;
 }
 
 int main(){
 	int x,y;
-  float r;
-  printf("Introduza dois números: ");
+	float r;
+	printf("Introduza dois números: ");
 	scanf("%d %d", &x, &y);
-  calc(x,y);
-  intswap(&x, &y);
-  calc(x,y);
+	calc(x,y);
+	intswap(&x, &y);
+	calc(x,y);
 	return 0;
 }
