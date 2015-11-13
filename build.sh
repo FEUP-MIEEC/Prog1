b729a631f3336d0dcfbaeb97d44e2e2169def05e
Daniel Silva <daniel_p_silva_97@hotmail.com>
2015-11-13 19:05:17 +0000

    Try again

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 echo "Begin Building:"
 
-for SCRIPT in build/*
-	do
-		if [ -f $SCRIPT -a -x $SCRIPT ]
-		then
-			$SCRIPT
-		fi
-	done
+for file in /build/*
+do
+  cmd $file
+done
--- build/p01_p01.sh
+++ build/p01_p01.sh
@@ -1,1 +1,2 @@
 echo "1 here!"
+pause
