338972fea1fa8544f037a5e2b245f6a45642f02c
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-11-14 18:17:17 +0000

    Otimizações - Aula 2 - Problema 6

--- Aula_Pratica_02/Problema_6.c
+++ Aula_Pratica_02/Problema_6.c
@@ -1,37 +1,15 @@
 /*
  *  Created on: 28/10/2015
- *      Author: Gonçalo Pereira
+ *      Author: Gonçalo Pereira, Fábio Gaspar
  */
-// ESTE PROGRAMA FUNCIONA COMO ESPERADO, MAS NÃO ESTÁ IMPLEMENTADO COMO PEDIDO. POR FAVOR COMUNIQUE SE SOUBER A RESPOSTA
+
 #include <stdio.h>
 
 int main(){
-	int n;
+	float n;
+	int n2;
 	printf("Insere um número ");
-	scanf("%d", &n);
-	printf("%d %d\n", ((2*n)+1)/2, ((2*n)-1)/2;
+	scanf("%f", &n);
+	n2 = n+.5;
+	printf("%d %d\n", n2+2 - n2%2, n2-2 + n2%2);
 }
-
-// int main(){
-// 	double numero;
-// 	int inteiro;
-
-// 	printf("Insere um número ");
-// 	scanf("%lf", &numero);
-	
-// 	inteiro = numero;
-
-// 	while((inteiro+1)%2 == 0){
-// 		inteiro++;
-// 	}
-
-// 	printf("%d ", inteiro);
-// 	inteiro = numero;
-
-
-// 	while((inteiro-1)%2 == 0){
-// 		inteiro--;
-// 	}
-// 	printf("%d \n", inteiro);
-// return 0;
-// }
