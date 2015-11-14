b33db38749c15c364d94d99abb9e34d99e4242f2
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-11-14 12:50:58 +0000

    Problema dos pares mais próximos ainda em falta

--- Aula_Pratica_02/Problema_6.c
+++ Aula_Pratica_02/Problema_6.c
@@ -6,25 +6,32 @@
 #include <stdio.h>
 
 int main(){
-	double numero;
-	int inteiro;
-
+	int n;
 	printf("Insere um número ");
-	scanf("%lf", &numero);
-	
-	inteiro = numero;
-
-	while((inteiro+1)%2 == 0){
-		inteiro++;
-	}
-
-	printf("%d ", inteiro);
-	inteiro = numero;
-
-
-	while((inteiro-1)%2 == 0){
-		inteiro--;
-	}
-	printf("%d \n", inteiro);
-return 0;
+	scanf("%d", &n);
+	printf("%d %d\n", ((2*n)+1)/2, ((2*n)-1)/2;
 }
+
+// int main(){
+// 	double numero;
+// 	int inteiro;
+
+// 	printf("Insere um número ");
+// 	scanf("%lf", &numero);
+	
+// 	inteiro = numero;
+
+// 	while((inteiro+1)%2 == 0){
+// 		inteiro++;
+// 	}
+
+// 	printf("%d ", inteiro);
+// 	inteiro = numero;
+
+
+// 	while((inteiro-1)%2 == 0){
+// 		inteiro--;
+// 	}
+// 	printf("%d \n", inteiro);
+// return 0;
+// }
