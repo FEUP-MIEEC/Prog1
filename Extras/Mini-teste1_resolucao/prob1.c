d9f595d0dd4e2067191b7a9c3153071be3d99cc7
Daniel Silva <daniel_p_silva_97@hotmail.com>
2015-11-16 11:20:29 +0000

    Submissão preliminar da resolução do exercício 1 do mini-teste 1

---
+++ Extras/Mini-teste1_resolucao/prob1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int videal, vmax, vmin, v, k=0, vtotal=0, i, cexc=0, cdef=0;
+	scanf("%d %d %d", &videal, &vmin, &vmax);
+	for (i=1;i<=50;i++){
+		scanf("%d", &v);
+		if (v>vmax){
+			cexc++;
+			continue;
+		}
+		if(v<vmin){
+			cdef++;
+			continue;
+		}
+		k++;
+		vtotal+=v;
+	}
+	float vmedio=(float)vtotal/k;
+	printf("Valor médio: %.2f (desvio absoluto de %.2f)\n", vmedio, fabs(vmedio-videal)/videal*100);
+	printf("Garrafas com volume em excesso: %d", cexc);
+	printf("Garrafas com volume em defeito: %d", cdef);
+}
