fe9698630b93d821283af5e51c3f9eaf95ff9e56
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-11-16 12:19:50 +0000

    Problema 2.2 do mini-teste submetido!

---
+++ Extras/Mini-teste1_resolucao/prob22.c
@@ -0,0 +1,38 @@
+/*
+ *  Created on: 16/11/2015
+ *      Author: Gonçalo Pereira
+ */
+#include <stdio.h>
+#include <math.h>
+
+void ConverteBinario(int num){
+	printf("%d	", );
+	for(i=8, i!=-1, i--){
+		if(pow(2, i)>num){
+			num-=pow(2, i);
+			printf("1");
+		}else{
+			printf("0");
+		}
+	}
+	printf("\n");
+}
+
+int main(){
+	int n;
+	char a;
+
+	while(scanf("%d %c", &n, &a) == 2){
+		switch(a){
+			case 'b':
+			ConverteBinario(n);
+			break;
+			case 'o':
+			printf("%d	%o\n", n, n);
+			break;
+			case 'h':
+			printf("%d	%x\n", n, n); //entre os valores pode ser também usado \t
+			break;
+		}
+	}
+}
