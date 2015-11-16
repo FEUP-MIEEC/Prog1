a2fb1b941205f8f12366a49396323d70213d2b95
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-11-16 12:18:51 +0000

    Problema 2.1 do mini-teste submetido!

---
+++ Extras/Mini-teste1_resolucao/prob21.c
@@ -0,0 +1,24 @@
+/*
+ *  Created on: 16/11/2015
+ *      Author: Gonçalo Pereira
+ */
+#include <stdio.h>
+
+int main(){
+	int n;
+	char a;
+
+	while(scanf("%d %c", &n, &a) == 2){
+		switch(a){
+			case 'b':
+			printf("%d	Conversão Indisponível\n", n);
+			break;
+			case 'o':
+			printf("%d	%o\n", n, n);
+			break;
+			case 'h':
+			printf("%d	%x\n", n, n); //entre os valores pode ser também usado \t
+			break;
+		}
+	}
+}
