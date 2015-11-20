7c1a854cd4e632a3ad24c2f2f0407f12944e4eac
Daniel Silva <daniel_p_silva_97@hotmail.com>
2015-11-20 15:29:34 +0000

    Submissão de novo problema

---
+++ Aula_Pratica_07/Problema_4.c
@@ -0,0 +1,57 @@
+/*
+ *  Created on: 20/11/2015
+ *      Author: Daniel Silva
+ *
+ * Com erros!
+ *
+ */
+ 
+ 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int gerar(int l, int h){
+	srand(time(NULL)*rand());
+  return rand()%h+l;
+}
+
+int main(){
+	int a, b, op, status=0, r, crp;
+	while (1)
+	{
+		a=gerar(1,100);
+		b=gerar(1,100);
+		op=gerar(1,2);
+		switch(op)
+		{
+			case 1:
+				while(status==0){
+					printf("Quanto é %d mais %d? ", a, b);
+					scanf("%d", &r);
+					if(r==a+b){
+						status=1;
+						printf("Muito Bem!\n");
+						crp++;
+					}else{
+						printf("Está errado, tenta outra vez!\n");
+					}
+				}
+			break;
+			case 2:
+			while(status==0){
+				printf("Quanto é %d vezes %d? ", a, b);
+				scanf("%d", &r);
+				if(r==a*b){
+					status=1;
+					printf("Muito Bem!\n");
+					crp++;
+				}else{
+					printf("Está errado, tenta outra vez!\n");
+				}
+			}
+			break;
+		}
+	}
+}
