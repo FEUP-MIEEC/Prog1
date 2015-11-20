5b35f296d4ead09c7c1684b975b00c9be94aec30
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-11-20 11:20:20 +0000

    Quase bem :toilet:

--- Aula_Pratica_07/Problema_1.c
+++ Aula_Pratica_07/Problema_1.c
@@ -6,10 +6,10 @@
 #include <stdio.h>
 #include <math.h>
 
-int fatorial(int n){
-	int fact;
+double fatorial(int n){
+	int fact = 1;
 	while(n>1){
-		fact *= 2*n-1;
+		fact *= (2*n)-1;
 		n--;
 	}
 	return fact;
@@ -17,19 +17,20 @@ int fatorial(int n){
 
 int main(){
 
-	double x, tol, dif, sen, serie, serie0;
+	double x, tol, dif = 99999999999, serie0=0, sen=0, serie=0;
 
 	printf("Qual o valor de x? ");
 	scanf("%lf", &x);
 	printf("Qual o valor da tolerância? ");
 	scanf("%lf", &tol);
 
-	for(int n=1; fabs(dif) < tol; n++){
-		serie = pow(-1, n) * pow(x, 2*n+1)/(fatorial(n));
+	for(int n=1; dif > tol; n++){
+		serie = pow(-1, n+1) * (pow(x, 2*n-1)/(fatorial(n)));
+		printf("%lf\n", serie);
 		sen += serie;
-		dif = serie - serie0;
+		dif = fabs(serie0 + serie);
 		serie0 = serie;
 	}
-	printf("O seno de %f é %f\n", x, sen);
+	printf("O seno de %lf é %.6lf\n", x, sen);
 	return 0;
 }
