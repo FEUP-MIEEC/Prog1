ea85db3f8b820f78a61b95eb36fa0951688065ed
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-11-28 11:28:28 +0000

    Aula 8 - Simulation Completely Optimized and Fully Working!

--- Aula_Pratica_08/PROG1_1415_P08/sfs/main.c
+++ Aula_Pratica_08/PROG1_1415_P08/sfs/main.c
@@ -19,137 +19,137 @@ void* maquinaEstado(void* args)
 	while(1)
 	{
 		/*---------------------Coloque aqui o seu código-----------------------*/
-		switch(state)
-		{
-			case 1:
-			if (tapeteLivre(SAT1) && !lerSensorTapete(SAT1,PSENSOR))
-			{
-				moveTapete(AT1,NEG);
-				moveTapete(SAT1,NEG);
-				state=2;
-			}
-			break;
-			case 2:
-			if (lerSensorTapete(SAT1,PSENSOR))
-			{
-				moveTapete(AT1,STOP);
-				moveTapete(SAT1,STOP);
-				state=3;
-			}
-			break;
-			case 3:
-				if(lerSensorTapete(SAT2, POS)){
-					rodaTapete(SAT2, NEG);
-				}
-				if(lerSensorTapete(SAT2, NEG)){
-						rodaTapete(SAT2, STOP);
-						moveTapete(SAT1,NEG);
-						moveTapete(SAT2,NEG);
-					state = 4;
-					}
-			break;
-			case 4:
-			if (lerSensorTapete(SAT2,PSENSOR))
-			{
-				moveTapete(SAT2,STOP);
-				if(lerSensorTapete(SAT2, NEG)){
-					rodaTapete(SAT2, POS);
-					state = 5;
-				}
-			}
-			break;
-			case 5:
-			if(lerSensorTapete(SAT2, POS)){
-					rodaTapete(SAT2, STOP);
-					moveTapete(SAT2, POS);
-					state = 6;
-				}
-			break;
-			case 6:
-			moveTapete(SAT3, POS);
-			if(lerSensorTapete(SAT3, PSENSOR)){
-				moveTapete(SAT3, STOP);
-				state = 7;
-			}
-			break;
-			case 7:
-			inicializaMaquina(MAQ1);
-			ligaMaquina(MAQ1);
-			temporizador(15);
-			desligaMaquina(MAQ1);
-			state = 8;
-			break;
-			case 8:
-			moveTapete(SAT3, POS);
-			moveTapete(SAT4, POS);
-			moveTapete(SAT5, POS);
-			state = 9;
-			break;
-			case 9:
-			if(lerSensorTapete(SAT5, PSENSOR)){
-				moveTapete(SAT5, STOP);
-				state = 10;
-			}
-			break;
-			case 10:
-			inicializaMaquina(MAQ2);
-			ligaMaquina(MAQ2);
-			temporizador(10);
-			desligaMaquina(MAQ2);
-			state = 11;
-			break;
-			case 11:
-			if (lerSensorTapete(SAT6, NEG)){
-				rodaTapete(SAT6, POS);
-			}
-			if(lerSensorTapete(SAT6, POS)){
-				rodaTapete(SAT6, STOP);
-				moveTapete(SAT5, POS);
-				state = 12;
-			}
-			break;
-			case 12:
-			moveTapete(SAT6, POS);
-			if(lerSensorTapete(SAT6, PSENSOR)){
-				moveTapete(SAT6, STOP);
-				state = 13;
-			}
-			break;
-			case 13:
-			rodaTapete(SAT6, NEG);
-			if(lerSensorTapete(SAT6, NEG)){
-				rodaTapete(SAT6, STOP);
-				state = 14;
-			}
-			break;
-			case 14:
-			moveTapete(SAT6, POS);
-			moveTapete(CT1, POS);
-			state = 15;
-			break;
-			case 15:
-			if(lerSensorTapete(CT1, PSENSOR)){
-				moveTapete(CT1, STOP);
-				state = 16;
-			}
-			break;
-			case 16:
-			if(!lerSensorPusher(PUSHER1, PSENSOR)){
-				empurraPeca(PUSHER1, NEG);
-			}else{
-                state = 17;
-            }
-			break;
-			case 17:
-			if(lerSensorPusher(PUSHER1, PSENSOR)){
-				empurraPeca(PUSHER1, STOP);
-				empurraPeca(PUSHER1, POS);
-			}
-			if(lerSensorPusher(PUSHER1, POS)){
-				empurraPeca(PUSHER1, STOP);
-			}
-			break;
-		}
+        switch(state)
+        {
+            case 1:
+                if (tapeteLivre(SAT1) && !lerSensorTapete(SAT1, PSENSOR)) {
+                    moveTapete(AT1, NEG);
+                    moveTapete(SAT1, NEG);
+                    state = 2;
+                }
+                break;
+            case 2:
+                if (lerSensorTapete(SAT1, PSENSOR)) {
+                    moveTapete(AT1, STOP);
+                    moveTapete(SAT1, STOP);
+                    state = 3;
+                }
+                break;
+            case 3:
+                if (lerSensorTapete(SAT2, POS)) {
+                    rodaTapete(SAT2, NEG);
+                }
+                if (lerSensorTapete(SAT2, NEG)) {
+                    rodaTapete(SAT2, STOP);
+                    moveTapete(SAT1, NEG);
+                    moveTapete(SAT2, NEG);
+                    state = 4;
+                }
+                break;
+            case 4:
+                if (lerSensorTapete(SAT2, PSENSOR)) {
+                    moveTapete(SAT2, STOP);
+                    if (lerSensorTapete(SAT2, NEG)) {
+                        rodaTapete(SAT2, POS);
+                        state = 5;
+                    }
+                }
+                break;
+            case 5:
+                if (lerSensorTapete(SAT2, POS)) {
+                    rodaTapete(SAT2, STOP);
+                    moveTapete(SAT2, POS);
+                    state = 6;
+                }
+                break;
+            case 6:
+                moveTapete(SAT3, POS);
+                if (lerSensorTapete(SAT3, PSENSOR)) {
+                    moveTapete(SAT3, STOP);
+                    state = 7;
+                }
+                break;
+            case 7:
+                inicializaMaquina(MAQ1);
+                ligaMaquina(MAQ1);
+                temporizador(15);
+                desligaMaquina(MAQ1);
+                state = 8;
+                break;
+            case 8:
+                moveTapete(SAT3, POS);
+                moveTapete(SAT4, POS);
+                if (lerSensorTapete(SAT5, PSENSOR)) {
+
+                }
+                moveTapete(SAT5, POS);
+                state = 9;
+                break;
+            case 9:
+                if (lerSensorTapete(SAT5, PSENSOR)) {
+                    moveTapete(SAT5, STOP);
+                    state = 10;
+                }
+                break;
+            case 10:
+                inicializaMaquina(MAQ2);
+                ligaMaquina(MAQ2);
+                temporizador(10);
+                desligaMaquina(MAQ2);
+                state = 11;
+                break;
+            case 11:
+                if (lerSensorTapete(SAT6, NEG)) {
+                    rodaTapete(SAT6, POS);
+                }
+                if (lerSensorTapete(SAT6, POS)) {
+                    rodaTapete(SAT6, STOP);
+                    moveTapete(SAT5, POS);
+                    state = 12;
+                }
+                break;
+            case 12:
+                moveTapete(SAT6, POS);
+                if (lerSensorTapete(SAT6, PSENSOR)) {
+                    moveTapete(SAT6, STOP);
+                    state = 13;
+                }
+                break;
+            case 13:
+                rodaTapete(SAT6, NEG);
+                if (lerSensorTapete(SAT6, NEG)) {
+                    rodaTapete(SAT6, STOP);
+                    state = 14;
+                }
+                break;
+            case 14:
+                moveTapete(SAT6, POS);
+                moveTapete(CT1, POS);
+                state = 15;
+                break;
+            case 15:
+                if (lerSensorTapete(CT1, PSENSOR)) {
+                    moveTapete(CT1, STOP);
+                    state = 16;
+                }
+                break;
+            case 16:
+                if (!lerSensorPusher(PUSHER1, PSENSOR)) {
+                    empurraPeca(PUSHER1, NEG);
+                } else {
+                    state = 17;
+                }
+                break;
+            case 17:
+                if (lerSensorPusher(PUSHER1, PSENSOR)) {
+                    empurraPeca(PUSHER1, STOP);
+                    empurraPeca(PUSHER1, POS);
+                }
+                if (lerSensorPusher(PUSHER1, POS)) {
+                    empurraPeca(PUSHER1, STOP);
+                }
+                break;
+        }
         
 
         /*---------------Não acrescente código para além deste ponto-----------*/
