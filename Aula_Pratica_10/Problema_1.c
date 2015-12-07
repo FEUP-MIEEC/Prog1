57b30b6e35bf97df13ecb24dfc246d0d2145393d
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-12-07 22:31:02 +0000

    Inicialized Aula 10

--- Aula_Pratica_10/Problema_1.c
+++ Aula_Pratica_10/Problema_1.c
@@ -2,8 +2,6 @@
 // Created by Goncalo on 05/12/2015.
 //
 
-#include <stdio.h>
-
 int main(){
-    printf("Hello World");
+    return 0;
 }
--- Aula_Pratica_10/Problema_2.c
+++ Aula_Pratica_10/Problema_2.c
@@ -2,3 +2,6 @@
 // Created by Goncalo on 05/12/2015.
 //
 
+int main() {
+    return 0;
+}
