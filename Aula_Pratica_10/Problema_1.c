fec2020383ba3d34b1ed8bd8af72da441817cb9a
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-12-11 14:43:19 +0000

    Aula 10 Exercicio 1

--- Aula_Pratica_10/Problema_1.c
+++ Aula_Pratica_10/Problema_1.c
@@ -8,20 +8,33 @@ int procuraTodos(int *v, int N, int x, int *posicoes, int *nPosicoes);
 
 int main()
 {
-    int v[100], N, x, posicoes[100], nPosicoes, i;
+    int v[100], N, x, posicoes[100], nPosicoes = 0, i, z;
 
     printf("Qual é o tamanho do vetor? ");
     scanf("%d", &N);
     printf("Insira uma série de números: ");
-    for (i = 0; i < N; i++) {
-        printf("Insira o %dº número", i + 1);
-        scanf("%d", v[i]);
+    for (i = 0; i < N - 1; i++) {
+        printf("Insira o %dº número: ", i + 1);
+        scanf("%d", &v[i]);
     }
-
+    printf("Que valor pretende procurar? ");
+    scanf("%d", &x);
+    procuraTodos(&v, N, x, &posicoes, &nPosicoes);
+    printf("Foi encontrado o número %d vezes, nas posições ", nPosicoes);
+    for (z = 0; posicoes[z] != 0; ++z) {
+        printf("%d ", posicoes[z]);
+    }
     return 0;
 }
 
 int procuraTodos(int *v, int N, int x, int *posicoes, int *nPosicoes) {
-
-    return 0;
+    int j;
+    for (j = 0; j < N - 1; ++j) {
+        if (x == v[j]) {
+            posicoes[*nPosicoes] = j;
+            nPosicoes++;
+        }
+    }
+    if (nPosicoes > 0) return 1;
+    else return 0;
 }
