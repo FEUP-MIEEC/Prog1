edea58f4869b8a8b0351d2da8a6d429843f6942b
Fábio Gaspar <fabiodanielreis@hotmail.com>
2015-12-11 10:16:27 +0000

    Submissão de problemas

--- Aula_Pratica_10/Problema_1.c
+++ Aula_Pratica_10/Problema_1.c
@@ -1,7 +1,62 @@
-//
-// Created by Goncalo on 05/12/2015.
-//
+#include <stdio.h>
 
-int main(){
-    return 0;
+int procuraTodos(int *v, int N, int x, int *posicoes, int *nPosicoes);
+
+int main()
+{
+	int v[100], posicoes[100], N, x, nPosicoes, j=0;
+
+	printf("Quantos valores pretende inserir: ");
+	scanf("%d", &N);
+
+	printf("Introduza os valores\n");
+
+	while (j < N)
+	{
+		scanf("%d", &v[j]);
+		j++;
+	}
+	
+	printf("Qual o valor a procurar?:");
+	scanf("%d", &x);
+
+	/*Testar*/
+	if (procuraTodos(v, N, x, posicoes, &nPosicoes)) //Foram encontrados valores
+	{
+		printf("Foram encontradas %d correspondencias do numero %d\n", nPosicoes, x);
+		printf("Posicoes: ");
+		for (int i = 0; i < nPosicoes; ++i)
+		{
+			printf("%d ", posicoes[i]);
+		}
+		
+	}
+	else printf("Nao foi encontrado nenhum valor de %d\n", x);
+
+}
+
+int procuraTodos(int *v, int N, int x, int *posicoes, int *nPosicoes)
+{
+	*nPosicoes=0;
+	int j = 0;
+	for (int i = 0; i < N; ++i)
+	{
+
+		if (v[i]==x)
+		{
+			
+			posicoes[j]=i;
+			++*nPosicoes;
+
+			//Testar
+			printf("v[%d]=%d; posicoes[%d]=%d; npos %d\n",i,v[i],j,i, *nPosicoes);
+			
+			j++;
+
+		}
+	}
+
+	if (nPosicoes==0) return 0;
+	
+	else return 1;
 }
--- Aula_Pratica_10/Problema_2.c
+++ Aula_Pratica_10/Problema_2.c
@@ -1,7 +1,64 @@
-//
-// Created by Goncalo on 05/12/2015.
-//
+#include <stdio.h>
+#include <string.h>
 
-int main() {
-    return 0;
-}
+#define MAX 100
+
+ int compacta(char *orig, int N, char *dst);
+
+ int main()
+ {
+ 	char input[MAX], output[MAX];
+
+ 	printf("Escreva uma string com menos que %d caracteres: ", MAX-1);
+ 	fgets(input, MAX, stdin);
+
+ 	int a;
+ 	while(1)
+ 	{
+	 	a = (strlen(input) - 2); 
+		/*
+		Se o input for 'aaabbb.', char array tem tamanho 8, pois {'a','a','a','b','b','b','c','\0'}
+		Então, uma vez que o vetor começa no indice 0, decrementam-se duas unidades para ajustar os indices e outra para excluir o '\0'
+		*/
+		if(input[a] == '.') break;
+
+		//TODO
+ 	}
+ 	
+
+ 	compacta(input, a, output);
+
+ 	printf("Output: %s\n", output);
+ }
+
+  int compacta(char *orig, int N, char *dst)
+  {
+  	int flags[MAX] = {0};
+  	int i;
+  	for ( i = 0; i < N; ++i)
+  	{
+  		char aux = orig[i];
+  		int j=i+1;
+
+  		for (j ; j < N; ++j)
+  		{
+  			if (orig[j]==aux)
+  			{
+  				flags[j]=1;
+  			}
+  		}
+  	}
+
+  	int k=0;
+  	for (int i = 0; i < N; ++i)
+  	{
+  		if (flags[i]==0)
+  		{
+  			dst[k]=orig[i];
+  			k++;
+  		}
+  	}
+
+  	return k;
+
+  }
