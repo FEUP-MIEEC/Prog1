2592378bcb5bee2164f04be7076908719eafa7c8
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-12-13 14:17:40 +0000

    Make C as default repo language

--- Aula_Pratica_08/Simulação/sfs/install.sh
+++
@@ -1,7 +0,0 @@
-BASE_DIR=`pwd`
-mkdir modbus
-cd libmodbus-3.0.6
-./configure --prefix $BASE_DIR/modbus 
-make
-make install
-cd ..
--- Aula_Pratica_08/Simulação/sfs/sfs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-LD_LIBRARY_PATH=`pwd`/modbus/lib
-export LD_LIBRARY_PATH
-make clean
-make
-./sfs
