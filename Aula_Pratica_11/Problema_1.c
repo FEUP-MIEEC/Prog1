fa504b2a28889badbfe5c2d6dc52fdc8b815df99
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2015-12-15 22:47:48 +0000

    Aula 11 - Problema 1

---
+++ Aula_Pratica_11/Problema_1.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdio.h>
+
+#define M 100
+
+void inverte(char *strOriginal, char *strInvertida);
+
+int main() {
+    int i;
+    char f[M], inf[M];
+
+    fgets(f, sizeof(f), stdin);
+
+    inverte(f, inf);
+
+    for (i = 0; i < strlen(f); ++i) {
+        printf("%c", inf[i]);
+    }
+    return 0;
+}
+
+void inverte(char *strOriginal, char *strInvertida) {
+    int i, j = strlen(strOriginal) - 1;
+    for (i = 0; i < strlen(strOriginal); ++i) {
+        strInvertida[i] = strOriginal[j];
+        j--;
+    }
+}
