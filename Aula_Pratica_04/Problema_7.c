84967d17bcc5dc8ad58f5e69b4a31fdbec1ec8fd
Fábio Gaspar <fabiodanielreis@hotmail.com>
2016-01-04 20:43:24 +0000

    Submissão

---
+++ Aula_Pratica_04/Problema_7.c
@@ -0,0 +1,25 @@
+/*
+ *  Created on: 27/12/2015
+ *      Author: Fábio Gaspar
+ */
+#include <stdio.h>
+
+ int main()
+ {
+ 	unsigned int x, y, counter = 0;
+
+ 	printf("Insira um operando: ");
+ 	scanf("%d", &x);
+ 	printf("Insira outro operando (potencia de base 2): ");
+ 	scanf("%d", &y);
+
+ 	/*fatorizar o y, para saber quantos bits se vão deslocar*/
+ 	while(y%2==0)
+ 	{
+ 		counter++;
+ 		y/=2; 
+ 	}
+
+ 	printf("%d\n", x<<counter);
+
+ }
