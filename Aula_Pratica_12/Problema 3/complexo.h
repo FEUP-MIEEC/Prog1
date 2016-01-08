a1b7b5ee1646d3839c97330bd35ff7411b4e04a9
Fábio Gaspar <Hyrokumata@users.noreply.github.com>
2016-01-08 10:56:57 +0000

    Update complexo.h

--- Aula_Pratica_12/Problema 3/complexo.h
+++ Aula_Pratica_12/Problema 3/complexo.h
@@ -1,3 +1,22 @@
 /*
- *      Author: Fábio Gaspar
+ *      Author: FÃ¡bio Gaspar
  */
+#ifndef _COMPLEXOS_H
+#define _COMPLEXOS_H
+
+typedef struct {
+	int real, img;
+} complexo;
+
+complexo leComplexo();
+
+void escreveComplexo(complexo c);
+
+complexo somaComplexo(complexo c1, complexo c2);
+
+double modComplexo(complexo c);
+
+double argComplexo(complexo c);
+
+#endif
+
