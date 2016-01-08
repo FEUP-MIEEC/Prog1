6c9892b91fd2175ef6e0cc189114fa49f4e85105
Fábio Gaspar <Hyrokumata@users.noreply.github.com>
2016-01-08 10:56:35 +0000

    Update complexo.c

--- Aula_Pratica_12/Problema 3/complexo.c
+++ Aula_Pratica_12/Problema 3/complexo.c
@@ -1,3 +1,48 @@
 /*
- *      Author: F�bio Gaspar
+ *      Author: Fábio Gaspar
  */
+#include <stdio.h>
+#include <math.h>
+#include "complexo.h"
+
+complexo leComplexo()
+{
+	scanf("%d %d", complexo.real, complexo.img);
+	return complexo;
+}
+
+void escreveComplexo(complexo c)
+{
+	printf("%d + i%d", c.real, c.img);
+}
+
+complexo somaComplexo(complexo c1, complexo c2)
+{
+	complexo c;
+
+	c.real = c1.real + c2.real;
+	c.img = c1.img + c2.img;
+
+	return c;
+}
+
+double modComplexo(complexo c)
+{
+	/*
+	seja z = x + yi
+	|z|= sqrt(x² + y²)
+	*/
+
+	return sqrt(pow(x,2)+pow(y,2));
+}
+
+double argComplexo(complexo c)
+{
+	/*
+	seja z = x + yi
+	arg(z) = 'teta', tal que cos('teta') = x/|z| e sin('teta') = y/|z|
+	então, arg(z) = arccos(x/|z|) = arcsin(y/|z|)
+	*/
+
+	return acos(c.real / modComplexo(c));
+}
