d13dc68bb6f9574b7990a9818f5a7cbf867b02b9
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2016-01-15 14:03:29 +0000

    Aula 13 - Problema 1

--- Aula_Pratica_13/Problema_1/Problema_1.c
+++ Aula_Pratica_13/Problema_1/Problema_1.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 
 int main() {
-    int *v, j, i, *med, k;
-    v = (int *) malloc(1);
-    med = (int *) malloc(1);
-    for (j = 0; scanf("%d", &v[j]) == 1; j++) {
-        v = (int *) realloc(v, 1);
-        med = (int *) realloc(med, 1);
+    int *v, j = 0, i, *med, k, val;
+    while (scanf("%d", &val) == 1) {
+        if (j == 0)
+            v = (int *) malloc(sizeof(int));
+        else
+            v = (int *) realloc(v, sizeof(int) * (j + 1));
+        v[j] = val;
+        j++;
     }
+    med = (int *) malloc(sizeof(int) * (j + 1));
     for (i = 0; i < j; i++) {
         if (i == 0) {
             med[i] = (v[i] + v[i + 1]) / 2;
