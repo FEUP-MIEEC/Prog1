9c02191b9b070e8f95dd90700df40cc35cae9fc4
Fábio Gaspar <fabiodanielreis@hotmail.com>
2016-01-27 15:30:47 +0000

    Miniteste 2 Exemplo 1
    Ficheiros fornecidos na prova + Resolução (incompleta)

---
+++ Extras/Mini-teste2_exemplo_1/Resolução/Problema_1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int ler_volumes (float *volumes){
+	int n, i;
+	scanf("%d", &n);
+	for (i=0; i<=n; i++){
+		scanf("%f", &volumes[i]);
+	}
+	return n;
+}
+
+int volume_medio(float *volumes, int nvolumes, float max, float min){
+	int i, n=0;
+	float total=0;
+	for(i=0;i<nvolumes; i++){
+		if(volumes[i]>=min && volumes[i]<=max){
+			total+=volumes[i];
+			n++;
+		}
+	}
+	return total/n;
+}
+
+int main (){
+	float volumes[1000];
+	float maximo, minimo, media;
+	int n;
+	
+	n = ler_volumes(volumes);
+	scanf("%f", &minimo);
+	scanf("%f", &maximo);
+	media=volume_medio(volumes, n, maximo, minimo);
+	printf("Volume médio = %.3f", media);
+}
