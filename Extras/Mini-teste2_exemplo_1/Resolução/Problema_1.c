cac4ff9f3b18fb83e19dca4c60e29b98e4ebd1aa
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2016-01-28 14:48:34 +0000

    Error correction

--- Extras/Mini-teste2_exemplo_1/Resolução/Problema_1.c
+++ Extras/Mini-teste2_exemplo_1/Resolução/Problema_1.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 
-int ler_volumes (float *volumes){
-	int n, i;
-	scanf("%d", &n);
-	for (i=0; i<n; i++){
-		scanf("%f", &volumes[i]);
-	}
-	return n;
+int ler_volumes(float *volumes) {
+    int n, i;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        scanf("%f", &volumes[i]);
+    }
+    return n;
 }
 
-float volume_medio(float *volumes, int nvolumes, float max, float min){
-	int i, n=0;
-	float total=0;
-	for(i=0;i<nvolumes; i++){
-		if(volumes[i]>=min && volumes[i]<=max){
-			total+=volumes[i];
-			n++;
-		}
-	}
-	
-	return total/n;
+float volume_medio(float *volumes, int nvolumes, float max, float min) {
+    int i, k = 0;
+    float sum = 0;
+    for (i = 0; i < nvolumes; i++) {
+        if (volumes[i] >= min && volumes[i] <= max) {
+            sum += volumes[i];
+            k++;
+        }
+    }
+    return sum / (float) k;
 }
 
-int main (){
-	float volumes[1000];
-	float maximo, minimo, media;
-	int n;
-	
-	n = ler_volumes(volumes);
-	scanf("%f", &minimo);
-	scanf("%f", &maximo);
-	media=volume_medio(volumes, n, maximo, minimo);
-	printf("Volume médio = %.3f\n", media);
+int main() {
+    float v[1000];
+    float min, max;
+    int n = ler_volumes(v);
+    printf("Foram lidos %d valores\n", n);
+    scanf("%f", &min);
+    scanf("%f", &max);
+    printf("Indique o valor minimo: %f\n", min);
+    printf("Indique o valor maximo: %f\n", max);
+    printf("O volume medio de enchimento foi de %.3f litros\n", volume_medio(v, n, max, min));
 }
